35018d229aa07831345d25386aaa30bd598e1ff7
NoneType4Name <90710372+NoneType4Name@users.noreply.github.com>
2025-09-29 17:27:37 +0300

    cosmetics

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <Windows.h>
 #include <qobject.h>
 
 int main(int argc, char *argv[]) {
+
   QApplication a(argc, argv);
   MainWindow w;
   w.show();
-  QString sd{"1213232asd"};
   return a.exec();
 }
